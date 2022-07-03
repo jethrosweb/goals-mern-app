91027c84eeb15fda7a0c3601deafbbaec9074717
Jethro Dean <jethroldean@gmail.com>
2022-07-03 17:34:40 +1000

    error in previous push

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,38 +1,36 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const API_URL = '/api/users/'
 
-// Register user
+// register user
 const register = async (userData) => {
-  const response = await axios.post(API_URL, userData)
+    const response = await axios.post(API_URL, userData)
 
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
-  }
-
-  return response.data
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
 }
 
-// Login user
+// login user
 const login = async (userData) => {
-  const response = await axios.post(API_URL + 'login', userData)
+    const response = await axios.post(API_URL + 'login', userData)
 
-  if (response.data) {
-    localStorage.setItem('user', JSON.stringify(response.data))
-  }
-
-  return response.data
+    if (response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    }
+    return response.data
 }
 
-// Logout user
+// logout user
 const logout = () => {
-  localStorage.removeItem('user')
+    localStorage.removeItem('user')
 }
 
 const authService = {
-  register,
-  logout,
-  login,
+    register,
+    logout, 
+    login
 }
 
 export default authService
